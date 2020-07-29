7f1250cc1ad20d0bcc63bf31f0a6a9d880ffa2b6
Vidushi Bhardwaj <vidudhi1710@gmail.com>
2020-07-29 01:18:45 -0700

    <message>

---
+++ src/app/contact-create/contact-create.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'app-contact-create',
+  templateUrl: './contact-create.component.html',
+  styleUrls: ['./contact-create.component.scss']
+})
+export class ContactCreateComponent implements OnInit {
+  contact : {id, name, description, email} = {id: null, name: "", description: "", email: ""};
+  constructor(public dataService: DataService) { }
+  ngOnInit() {
+  }
+  createContact(){
+    console.log(this.contact);
+    this.dataService.createContact(this.contact);
+    this.contact = {id: null, name: "", description: "", email: ""};
+  }
+}
